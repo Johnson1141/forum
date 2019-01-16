d3f0d8490ad397edf57485efbc2328b3e9831b10
zsy <1486613863@qq.com>
2019-01-16 19:28:13 +0800

    new

--- forum/src/main/java/com/example/demo/ForumApplication.java
+++ forum/src/main/java/com/example/demo/ForumApplication.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class ForumApplication {
 
@@ -10,4 +12,9 @@ public class ForumApplication {
         SpringApplication.run(ForumApplication.class, args);
     }
 
+
+
+
+
 }
+
